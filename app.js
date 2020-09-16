d40e094b255dced277aebf4c44a7b489024ef65c
Vishesh Singh <VisheshSingh16@gmail.com>
2020-09-15 22:06:06 -0500

    status codes 💯

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const xhr = new XMLHttpRequest();
 xhr.open('GET', `https://jsonplaceholder.typicode.com/todos`);
 
 xhr.addEventListener('readystatechange', () => {
-  if (xhr.readyState === 4) {
+  if (xhr.readyState === 4 && xhr.status === 200) {
     console.log(JSON.parse(xhr.responseText));
+  } else if (xhr.readyState === 4) {
+    console.log('Could not fetch data');
   }
 });
 
