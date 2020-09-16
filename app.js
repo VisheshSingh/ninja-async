fa404e9ac65a2361672606f355adc634ee002e84
Vishesh Singh <VisheshSingh16@gmail.com>
2020-09-16 18:22:11 -0500

    callback fn 🤙

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const xhr = new XMLHttpRequest();
+const getTodos = (callback) => {
+  const xhr = new XMLHttpRequest();
 
-xhr.open('GET', `https://jsonplaceholder.typicode.com/todos`);
+  xhr.open('GET', `https://jsonplaceholder.typicode.com/todos`);
 
-xhr.addEventListener('readystatechange', () => {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    console.log(JSON.parse(xhr.responseText));
-  } else if (xhr.readyState === 4) {
-    console.log('Could not fetch data');
+  xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(undefined, JSON.parse(xhr.responseText));
+    } else if (xhr.readyState === 4) {
+      callback('Could not fetch data', undefined);
+    }
+  });
+
+  xhr.send();
+};
+
+console.log(1);
+console.log(2);
+getTodos((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
-
-xhr.send();
+console.log(3);
+console.log(4);
